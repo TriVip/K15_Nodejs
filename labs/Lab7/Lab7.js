d4d44068aaf6d454661a114fa38eb4445af1520f
S3CORP\phuong.trinh <phuong.trinh@s3corp.com.vn>
2024-08-14 12:13:50 +0700

    Submit Lab7

--- labs/Lab7/Lab7.js
+++ labs/Lab7/Lab7.js
@@ -2,18 +2,18 @@ const Post = require("./Post");
 const RequestHandler = require("./RequestHandler");
 
 async function Lab7() {
-    // Given params
+    
     const userId = 1;
     const postId = 5;
 
-    // Create RequestHandler object
+    
     const requestHandler = new RequestHandler();
 
-    // Fetch and print the target post
+    
     const post = await requestHandler.printTargetPost(userId, postId);
     console.log('Target Post:', post);
 
-    // Fetch and print all posts for the user
+    
     const allPosts = await requestHandler.printAllPosts(userId);
     console.log('All Posts:', allPosts);
 }
--- labs/Lab7/RequestHandler.js
+++ labs/Lab7/RequestHandler.js
@@ -1,7 +1,6 @@
 const Post = require('./Post');
 
 class RequestHandler {
-
     async printTargetPost(userId, postId) {
         const allPosts = await this._getAllPosts(userId);
         const targetPost = allPosts.find(post => post.id === postId);
@@ -16,8 +15,11 @@ class RequestHandler {
     async _getAllPosts(userId) {
         try {
             const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
-            const postsData = response.data;
-            return postsData.map(post => new Post(post._userId, post._id, post._title, post._body));
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const postsData = await response.json();
+            return postsData.map(post => new Post(post.userId, post.id, post.title, post.body));
         } catch (error) {
             console.error('Error fetching posts:', error);
             return [];
